698574130b80e6e1efa0d4d491019c1ff29c7439
Sérgio Santos <me@s3rgiosan.com>
2024-01-26 10:33:02 +0000

    Add compiled files for testing

---
+++ build/ptwoo-nif-block-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6adbe6d3eef95e8a6606');
---
+++ build/ptwoo-nif-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8aef5fd8b6d4b8ea1b7a');
