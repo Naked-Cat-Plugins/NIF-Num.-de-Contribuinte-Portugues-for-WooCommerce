41dd096efeb81ac40830a8b3013f5ea4dffe88ec
Sérgio Santos <me@s3rgiosan.com>
2024-01-26 12:06:39 +0000

    Move block to before the Shipping Options block

--- build/ptwoo-nif-block-frontend.asset.php
+++ build/ptwoo-nif-block-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6adbe6d3eef95e8a6606');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '98f059ccc7802644ada3');
