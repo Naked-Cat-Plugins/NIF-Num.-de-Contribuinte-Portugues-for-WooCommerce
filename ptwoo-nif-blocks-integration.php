1c996e1543a9ac97607425fcc49879ff95d21cae
Marco Almeida <marcoalmeida@webdados.pt>
2024-01-31 15:12:52 +0000

    Fix ptwoo-nif-block-frontend.js translations

--- ptwoo-nif-blocks-integration.php
+++ ptwoo-nif-blocks-integration.php
@@ -118,8 +118,7 @@ class PTWoo_NIF_Blocks_Integration implements IntegrationInterface {
 
 		wp_set_script_translations(
 			'ptwoo-nif-block-frontend',
-			'nif',
-			PTWOO_NIF_PLUGIN_DIR . 'lang'
+			'nif-num-de-contribuinte-portugues-for-woocommerce'
 		);
 	}
 
